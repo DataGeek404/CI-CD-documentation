7f0c7f61439a96a828c62c859d63257232911138
DataGeek404 <techjayprivate@gmailcom>
2025-08-17 16:26:29 +0300

    ci/cd docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -73,7 +73,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'ci/cd Docs',
+      title: 'DevOps Documentation',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
