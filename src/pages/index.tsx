a2057a96a5fc82b77c3e9495fd2caf8499876665
DataGeek404 <techjayprivate@gmailcom>
2025-08-17 15:30:58 +0300

    ci/cd docs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`DataGeek404 from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
